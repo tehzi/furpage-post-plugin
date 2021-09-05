dfe39545129567d7d8a85dd6c53be13c981272e5
Mikhail Zi <zi.white.drago@gmail.com>
2021-09-06 01:07:18 +0300

    Add legacy plugin code for future fix #8

--- packages/plugin-front-legacy/webpack.config.js
+++ packages/plugin-front-legacy/webpack.config.js
@@ -39,10 +39,10 @@ const config = {
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export default (environment, { mode }) => ({
     devtool: isDevelopment(mode) && "inline-source-map",
-    optimization: {
-        namedModules: true, // NamedModulesPlugin()
-        noEmitOnErrors: true, // NoEmitOnErrorsPlugin
-    },
+    // optimization: {
+    //     namedModules: true, // NamedModulesPlugin()
+    //     noEmitOnErrors: true, // NoEmitOnErrorsPlugin
+    // },
     entry: {
         "furpage-bundle": "./src/js/pages/bundle.tsx",
         background: "./src/js/pages/background.tsx",
@@ -54,76 +54,76 @@ export default (environment, { mode }) => ({
     },
     module: {
         rules: [
-            {
-                test: /\.[jt]sx?/,
-                exclude: /node_modules/,
-                loader: "happypack/loader?id=ts",
-            },
-            {
-                test: /.*\.(ttf|eot|svg|css|woff|woff2|png|ico|jpg|jpeg|gif)$/i,
-                use: [
-                    {
-                        loader: "file-loader",
-                        options: {
-                            name: "[name].[ext]",
-                            outputPath: "./",
-                            useRelativePath: false,
-                        },
-                    },
-                ],
-            },
+            // {
+            //     test: /\.[jt]sx?/,
+            //     exclude: /node_modules/,
+            //     loader: "happypack/loader?id=ts",
+            // },
+            // {
+            //     test: /.*\.(ttf|eot|svg|css|woff|woff2|png|ico|jpg|jpeg|gif)$/i,
+            //     use: [
+            //         {
+            //             loader: "file-loader",
+            //             options: {
+            //                 name: "[name].[ext]",
+            //                 outputPath: "./",
+            //                 useRelativePath: false,
+            //             },
+            //         },
+            //     ],
+            // },
         ],
     },
     plugins: compact([
-        new CleanWebpackPlugin({}),
-        new Dotenv({
-            path: `./.env.${isDevelopment ? "local" : "prod"}`,
-            safe: true,
-        }),
-        new CopyWebpackPlugin([
-            {
-                from: "./src/json/**/*",
-                to: "./",
-                flatten: true,
-                // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-                transform(content, path) {
-                    if (path.match(/manifest\.json$/)) {
-                        return content
-                            .toString()
-                            .replace("%%VERSION%%", packageJson.version);
-                    }
-                    return content;
-                },
-            },
-        ]),
-        new HappyPack({
-            id: "ts",
-            threads: 4,
-            loaders: compact([
-                {
-                    path: "babel-loader",
-                    exclude: [/node_modules/],
-                },
-                !isDevelopment(mode) && "eslint-loader",
-            ]),
-        }),
-        new HtmlPlugin(config.html),
-        new HtmlPlugin(config.popup),
-        !isDevelopment(mode) &&
-            new FileManager({
-                onEnd: [
-                    {
-                        mkdir: ["./release"],
-                    },
-                    {
-                        archive: [
-                            {
-                                source: config.path,
-                                destination: `${__dirname}/release/${packageJson.name}-${packageJson.version}.zip`,
-                            },
-                        ],
-                    },
-                ],
-            }),
+        // new CleanWebpackPlugin({}),
+        // new Dotenv({
+        //     path: `./.env.${isDevelopment ? "local" : "prod"}`,
+        //     safe: true,
+        // }),
+        // new CopyWebpackPlugin([
+        //     {
+        //         from: "./src/json/**/*",
+        //         to: "./",
+        //         flatten: true,
+        //         // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+        //         transform(content, path) {
+        //             if (path.match(/manifest\.json$/)) {
+        //                 return content
+        //                     .toString()
+        //                     .replace("%%VERSION%%", packageJson.version);
+        //             }
+        //             return content;
+        //         },
+        //     },
+        // ]),
+        // new HappyPack({
+        //     id: "ts",
+        //     threads: 4,
+        //     loaders: compact([
+        //         {
+        //             path: "babel-loader",
+        //             exclude: [/node_modules/],
+        //         },
+        //         !isDevelopment(mode) && "eslint-loader",
+        //     ]),
+        // }),
+        // new HtmlPlugin(config.html),
+        // new HtmlPlugin(config.popup),
+        // !isDevelopment(mode) &&
+        //     new FileManager({
+        //         onEnd: [
+        //             {
+        //                 mkdir: ["./release"],
+        //             },
+        //             {
+        //                 archive: [
+        //                     {
+        //                         source: config.path,
+        //                         destination: `${__dirname}/release/${packageJson.name}-${packageJson.version}.zip`,
+        //                     },
+        //                 ],
+        //             },
+        //         ],
+        //     }),
     ]),
 });
