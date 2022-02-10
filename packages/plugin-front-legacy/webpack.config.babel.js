74d35ca8a825225e3b210c35f29869be51aa540d
Mikhail Zi <zi.white.drago@gmail.com>
2022-02-10 16:27:49 +0300

    fix: Create MVP version of frontend plugin #11

--- packages/plugin-front-legacy/webpack.config.babel.js
+++ packages/plugin-front-legacy/webpack.config.babel.js
@@ -1,42 +1,38 @@
-// import CopyWebpackPlugin from "copy-webpack-plugin";
-// import { CleanWebpackPlugin } from "clean-webpack-plugin";
-// import FileManager from "filemanager-webpack-plugin";
-// import HtmlPlugin from "html-webpack-plugin";
-// import Dotenv from "dotenv-webpack";
+import CopyWebpackPlugin from "copy-webpack-plugin";
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
+import FileManager from "filemanager-webpack-plugin";
+import HtmlPlugin from "html-webpack-plugin";
+import Dotenv from "dotenv-webpack";
 import { TsconfigPathsPlugin } from "tsconfig-paths-webpack-plugin";
-// import { fileURLToPath } from "url";
-// import { dirname } from "path";
+import packageJson from "./package.json";
 
 const isDevelopment = mode => mode === "development";
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-
-// const config = {
-//     path: `${__dirname}/dist`,
-//     clean: {
-//         root: __dirname,
-//         verbose: true,
-//         dry: false,
-//     },
-//     html: {
-//         filename: "background.html",
-//         inject: false,
-//         template: `${__dirname}/src/html/background.html`,
-//     },
-//     popup: {
-//         filename: "popup.html",
-//         inject: false,
-//         template: `${__dirname}/src/html/popup.html`,
-//     },
-// };
+const config = {
+    path: `${__dirname}/dist`,
+    clean: {
+        root: __dirname,
+        verbose: true,
+        dry: false,
+    },
+    html: {
+        filename: "background.html",
+        inject: false,
+        template: `${__dirname}/src/html/background.html`,
+    },
+    popup: {
+        filename: "popup.html",
+        inject: false,
+        template: `${__dirname}/src/html/popup.html`,
+    },
+};
 
 export default (_, { mode }) => ({
     devtool: isDevelopment(mode) && "inline-source-map",
     entry: {
         "furpage-bundle": "./src/js/pages/bundle.tsx",
-        // background: "./src/js/pages/background.tsx",
-        // popup: "./src/js/pages/popup.tsx",
+        background: "./src/js/pages/background.tsx",
+        popup: "./src/js/pages/popup.tsx",
     },
     resolve: {
         extensions: [".js", ".json", ".jsx", ".ts", ".tsx"],
@@ -49,60 +45,61 @@ export default (_, { mode }) => ({
                 exclude: /node_modules/,
                 loader: "babel-loader",
             },
-        //     {
-        //         test: /.*\.(ttf|eot|svg|css|woff|woff2|png|ico|jpg|jpeg|gif)$/i,
-        //         use: [
-        //             {
-        //                 loader: "file-loader",
-        //                 options: {
-        //                     name: "[name].[ext]",
-        //                     outputPath: "./",
-        //                     useRelativePath: false,
-        //                 },
-        //             },
-        //         ],
-        //     },
+            {
+                test: /.*\.(ttf|eot|svg|css|woff|woff2|png|ico|jpg|jpeg|gif)$/i,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[ext]",
+                            outputPath: "./",
+                            useRelativePath: false,
+                        },
+                    },
+                ],
+            },
         ],
     },
     plugins: [
-        // new CleanWebpackPlugin(),
-        // new Dotenv({
-        //     path: `./.env.${isDevelopment ? "local" : "prod"}`,
-        //     safe: true,
-        // }),
-        // new CopyWebpackPlugin([
-        //     {
-        //         from: "./src/json/**/*",
-        //         to: "./",
-        //         flatten: true,
-        //         // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-        //         transform(content, path) {
-        //             if (path.match(/manifest\.json$/)) {
-        //                 return content
-        //                     .toString()
-        //                     .replace("%%VERSION%%", packageJson.version);
-        //             }
-        //             return content;
-        //         },
-        //     },
-        // ]),
-        // new HtmlPlugin(config.html),
-        // new HtmlPlugin(config.popup),
-        // !isDevelopment(mode) &&
-        //     new FileManager({
-        //         onEnd: [
-        //             {
-        //                 mkdir: ["./release"],
-        //             },
-        //             {
-        //                 archive: [
-        //                     {
-        //                         source: config.path,
-        //                         destination: `${__dirname}/release/${packageJson.name}-${packageJson.version}.zip`,
-        //                     },
-        //                 ],
-        //             },
-        //         ],
-        //     }),
+        new CleanWebpackPlugin(),
+        new Dotenv({
+            path: `./.env.${isDevelopment ? "local" : "prod"}`,
+            safe: true,
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: "./src/json/**/*",
+                    to: "[name][ext]",
+                    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+                    transform(content, path) {
+                        if (path.match(/manifest\.json$/)) {
+                            return content
+                                .toString()
+                                .replace("%%VERSION%%", packageJson.version);
+                        }
+                        return content;
+                    },
+                },
+            ],
+        }),
+        new HtmlPlugin(config.html),
+        new HtmlPlugin(config.popup),
+        !isDevelopment(mode) &&
+            new FileManager({
+                onEnd: [
+                    {
+                        mkdir: ["./release"],
+                    },
+                    {
+                        archive: [
+                            {
+                                source: config.path,
+                                destination: `${__dirname}/release/${packageJson.name}-${packageJson.version}.zip`,
+                            },
+                        ],
+                    },
+                ],
+            }),
     ].filter(Boolean),
 });
